398e3accc0eefdd62225f0bda0937f5d86cb85c9
Zeger Hendrikse <zegerh@yahoo.co.uk>
2026-05-19 13:56:56 +0200

    Added example 🛠️

--- examples/electromagnetism/main.js
+++ examples/electromagnetism/main.js
@@ -3,13 +3,15 @@ import { createChargedRingScene } from "./scenes/charged_ring.js";
 import { createDipoleFieldScene } from "./scenes/dipole_field.js";
 import { createElectromagneticWaveScene } from "./scenes/electromagnetic_wave_quiver.js"
 import { createAntennaScene } from "./scenes/antenna.js"
+import { createChargedRodScene } from "./scenes/charged_rod.js"
 
 const scenes = {
     solenoid: createSolenoidScene,
     dipole_field: createDipoleFieldScene,
     charged_ring: createChargedRingScene,
     electromagnetic_wave_quiver: createElectromagneticWaveScene,
-    antenna: createAntennaScene
+    antenna: createAntennaScene,
+    charged_rod: createChargedRodScene
 };
 
 if (document.getElementById("chargedRingCanvas"))
@@ -26,5 +28,8 @@ if (document.getElementById("electromagneticWaveCanvas"))
 
 if (document.getElementById("antennaCanvas"))
     scenes.antenna.run();
+
+if (document.getElementById("chargedRodCanvas"))
+    scenes.charged_rod.run();
 
 
