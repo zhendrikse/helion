cfda139e6458250104b23ea5ddb5903535c4a8f9
Zeger Hendrikse <zegerh@yahoo.co.uk>
2026-05-20 17:54:32 +0200

    Fix for GitHub pages 📜

--- vite.examples.config.js
+++ vite.examples.config.js
@@ -3,7 +3,7 @@ import path from 'path';
 
 export default defineConfig({
     build: {
-        outDir: 'public/examples',  // ← nu kopieert Astro dit automatisch
+        outDir: 'examples',
         emptyOutDir: true,
         rollupOptions: {
             input: {
