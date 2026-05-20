d8a43709703b645de09d149803f503251c907093
Zeger Hendrikse <zegerh@yahoo.co.uk>
2026-05-20 17:20:07 +0200

    Fix for GitHub pages 📜

--- vite.examples.config.js
+++ vite.examples.config.js
@@ -2,18 +2,19 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+    base: "/helion/",
     build: {
-        outDir: 'public/examples',  // waar de gebundelde scripts komen
+        outDir: 'dist/examples',  // waar de gebundelde scripts komen
         rollupOptions: {
             input: {
-                charged_ring: path.resolve(__dirname, 'examples/electromagnetism/scenes/charged_ring.js'),
-                charged_rod: path.resolve(__dirname, 'examples/electromagnetism/scenes/charged_rod.js'),
-                charged_sheet: path.resolve(__dirname, 'examples/electromagnetism/scenes/charged_sheet.js'),
-                dipole_field: path.resolve(__dirname, 'examples/electromagnetism/scenes/dipole_field.js'),
-                electromagnetic_wave_quiver: path.resolve(__dirname, 'examples/electromagnetism/scenes/electromagnetic_wave_quiver.js'),
-                n_body_oscillator: path.resolve(__dirname, 'examples/waves/scenes/n_body_oscillator.js'),
-                solenoid: path.resolve(__dirname, 'examples/electromagnetism/scenes/solenoid.js'),
-                star_cluster: path.resolve(__dirname, 'examples/astrophysics/scenes/star_cluster.js')
+                charged_ring: path.resolve(__dirname, 'public/examples/electromagnetism/scenes/charged_ring.js'),
+                charged_rod: path.resolve(__dirname, 'public/examples/electromagnetism/scenes/charged_rod.js'),
+                charged_sheet: path.resolve(__dirname, 'public/examples/electromagnetism/scenes/charged_sheet.js'),
+                dipole_field: path.resolve(__dirname, 'public/examples/electromagnetism/scenes/dipole_field.js'),
+                electromagnetic_wave_quiver: path.resolve(__dirname, 'public/examples/electromagnetism/scenes/electromagnetic_wave_quiver.js'),
+                n_body_oscillator: path.resolve(__dirname, 'public/examples/waves/scenes/n_body_oscillator.js'),
+                solenoid: path.resolve(__dirname, 'public/examples/electromagnetism/scenes/solenoid.js'),
+                star_cluster: path.resolve(__dirname, 'public/examples/astrophysics/scenes/star_cluster.js')
             },
             output: {
                 entryFileNames: '[name].js',
