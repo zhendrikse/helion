36caa89180e1e23a570e205a374cf73098bab5e2
Zeger Hendrikse <zegerh@yahoo.co.uk>
2026-05-20 16:01:05 +0200

    Fix for GitHub actions 8 🐞

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
-	base: '/helion/', // URL prefix for GitHub pages
+	base: '/', // URL prefix for GitHub pages
 	root: 'docs/dist',
 	integrations: [
 		starlight({
