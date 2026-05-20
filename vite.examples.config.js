27a6f847c25c9db056ce584ca13e9642ec374217
Zeger Hendrikse <zegerh@yahoo.co.uk>
2026-05-20 11:34:07 +0200

    Fix for GitHub pages 📜

--- vite.examples.config.js
+++ vite.examples.config.js
@@ -3,7 +3,7 @@ import path from 'path';
 
 export default defineConfig({
     build: {
-        outDir: 'public/examples',  // waar de gebundelde scripts komen
+        outDir: 'examples',  // waar de gebundelde scripts komen
         rollupOptions: {
             input: {
                 charged_ring: path.resolve(__dirname, 'examples/electromagnetism/scenes/charged_ring.js'),
