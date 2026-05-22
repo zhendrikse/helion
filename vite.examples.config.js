3723ff968f4e00c704ff214a89bfff3400ddb665
Zeger Hendrikse <zegerh@yahoo.co.uk>
2026-05-22 11:00:08 +0200

    Experiment textures ⚗️

--- vite.examples.config.js
+++ vite.examples.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 export default defineConfig({
     base: '/helion/',
     build: {
-        outDir: 'docs/public/examples',
+        outDir: 'docs/public',
         emptyOutDir: true,
         rollupOptions: {
             input: {
