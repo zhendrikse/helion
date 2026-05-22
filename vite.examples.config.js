69789e9a3d8394152608a6aaa82df781e510957e
Zeger Hendrikse <zegerh@yahoo.co.uk>
2026-05-22 10:48:18 +0200

    Fixing build 🐞

--- vite.examples.config.js
+++ vite.examples.config.js
@@ -4,6 +4,7 @@ import path from 'path';
 export default defineConfig({
     base: '/helion/',
     build: {
+        outDir: 'docs/public/examples',
         emptyOutDir: true,
         rollupOptions: {
             input: {
