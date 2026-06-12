de2467c01f84544a4a4e189b9ee8f343f60d8031
Zeger Hendrikse <zegerh@yahoo.co.uk>
2026-06-12 10:54:58 +0200

    Bug fix for uPlot graph 🪲

--- src/core/helion.js
+++ src/core/helion.js
@@ -1,4 +1,5 @@
-import uPlot from 'uplot';
+import uPlot from "uplot";
+import "uplot/dist/uPlot.min.css";
 
 /**********************************************
  * S I M U L A T I O N  E N V I R O N M E N T *
@@ -180,65 +181,5 @@ export class Simulation {
     onReset(resetFunction = () => {}) {
         this._onReset = resetFunction;
         return this
-    }
-}
-
-/*******************************************
- * G R A P H  ( u P L O T )  L I B R A R Y *
- *******************************************/
-
-export class UPlotGraph {
-    constructor({
-        plotDiv,
-        dataDefinition,
-        width = 600,
-        height = 300,
-        title="",
-        xLabel="",
-        yLabel="",
-        maxPoints = 500,
-        labelColor = "green",
-    } = {}) {
-        this._maxPoints = maxPoints;
-        this._graphData = [];
-        dataDefinition.forEach(() => this._graphData.push([]));
-
-        const series = [{}];
-        dataDefinition.slice(1).forEach(dataPoint => {
-            series.push({
-                label: dataPoint.label,
-                stroke: dataPoint.color
-            });
-        });
-
-        const uPlotOptions = this._uplotOptions(title, width, height, labelColor, xLabel, yLabel, series);
-        this._uplotChart = new uPlot(uPlotOptions, this._graphData, plotDiv);
-    }
-
-    _uplotOptions(title, width, height, labelColor, xLabel, yLabel, series) {
-        return { title, width, height,
-            bg: "transparent",
-            scales: { x: { auto: true }, y: { auto: true } },
-            axes: [{
-                    stroke: labelColor,
-                    font: "12px Arial",
-                    grid: { stroke: "rgba(255, 255, 255, 0.2)", width: 1 },
-                    label: xLabel,
-                }, {
-                    stroke: labelColor,
-                    font: "12px Arial",
-                    grid: { stroke: "rgba(255, 255, 255, 0.2)", width: 1 },
-                    label: yLabel
-                }],
-            series
-        };
-    }
-
-    get graphData() { return this._graphData; }
-
-    update() {
-        if (this._graphData[0].length > this._maxPoints)
-            this._graphData.forEach(arr => arr.shift());
-        this._uplotChart.setData(this._graphData);
     }
 }
---
+++ src/core/uplot.js
@@ -0,0 +1,58 @@
+import uPlot from "uplot";
+import "uplot/dist/uPlot.min.css";
+
+export class UPlotGraph {
+    constructor({
+        plotDiv,
+        dataDefinition,
+        width = 600,
+        height = 300,
+        title="",
+        xLabel="",
+        yLabel="",
+        maxPoints = 500,
+        labelColor = "green",
+    } = {}) {
+        this._maxPoints = maxPoints;
+        this._graphData = [];
+        dataDefinition.forEach(() => this._graphData.push([]));
+
+        const series = [{}];
+        dataDefinition.slice(1).forEach(dataPoint => {
+            series.push({
+                label: dataPoint.label,
+                stroke: dataPoint.color
+            });
+        });
+
+        const uPlotOptions = this._uplotOptions(title, width, height, labelColor, xLabel, yLabel, series);
+        this._uplotChart = new uPlot(uPlotOptions, this._graphData, plotDiv);
+    }
+
+    _uplotOptions(title, width, height, labelColor, xLabel, yLabel, series) {
+        return { title, width, height,
+            bg: "transparent",
+            scales: { x: { auto: true }, y: { auto: true } },
+            axes: [{
+                    stroke: labelColor,
+                    font: "12px Arial",
+                    grid: { stroke: "rgba(255, 255, 255, 0.2)", width: 1 },
+                    label: xLabel,
+                }, {
+                    stroke: labelColor,
+                    font: "12px Arial",
+                    grid: { stroke: "rgba(255, 255, 255, 0.2)", width: 1 },
+                    label: yLabel
+                }],
+            series
+        };
+    }
+
+    get graphData() { return this._graphData; }
+
+    update() {
+        if (this._graphData[0].length > this._maxPoints)
+            this._graphData.forEach(arr => arr.shift());
+        this._uplotChart.setData(this._graphData);
+    }
+}
