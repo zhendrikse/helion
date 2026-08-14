fb189b62f9503916abf1b81f9e5e402d3d17c507
Zeger Hendrikse <zegerh@yahoo.co.uk>
2026-08-14 15:27:18 +0200

    Bug fix 🐞

--- examples/mathematics/scenes/polar_coordinates_integration.js
+++ examples/mathematics/scenes/polar_coordinates_integration.js
@@ -1,10 +1,12 @@
 import { Box3, MathUtils } from "three";
 import {
-    Simulation, Vec3, Block, toCartesian, BlockSegments, BoxSegmentsView, Slider, Range
+    Simulation, Vec3, Block, toCartesian, BlockSegments, BoxSegmentsView, Slider, Range, Interval
 } from "../../../src/index.js";
 
 const R = 1, da = 0.05;
 const functionToIntegrate = (theta, phi) => theta * theta * (phi - Math.PI) * (phi - Math.PI);
+
+const integralValueDiv = document.createElement("div");
 
 class SegmentedSphere extends BlockSegments {
     constructor() {
@@ -19,9 +21,11 @@ class SegmentedSphere extends BlockSegments {
         for (let theta = 0; theta <= Math.PI; theta += da)
             for (let phi = 0; phi <= 2 * Math.PI; phi += da)
                 this.#createCell(theta, phi, values);
-        this._minF = Math.min(...values);
-        this._maxF = Math.max(...values);
+        this._interval = new Interval(Math.min(...values), Math.max(...values));
     }
+
+    updateIntegral = (value) =>
+        integralValueDiv.textContent = "Integral evaluates to: " + Number(value).toFixed(2);
 
     #createCell(theta, phi, values) {
         const val = functionToIntegrate(theta, phi);
@@ -44,38 +48,53 @@ class SegmentedSphere extends BlockSegments {
         );
     }
 
+    normalize = (value) => this._interval.normalize(value);
+
+    set thetaMax(value) {
+        this._thetaMax = value;
+        this.updateIntegral(this.integrate(functionToIntegrate));
+    }
+
+    set phiMin(value) {
+        this._phiMin = value;
+        this.updateIntegral(this.integrate(functionToIntegrate));
+    }
+
+    set phiMax(value) {
+        this._phiMax = value;
+        this.updateIntegral(this.integrate(functionToIntegrate));
+    }
+
     set thetaMin(value) {
         this._thetaMin = value;
-        console.log(this.integrate(functionToIntegrate));
+        this.updateIntegral(this.integrate(functionToIntegrate));
     }
+    get phiMin() { return this._phiMin; }
+    get phiMax() { return this._phiMax; }
+    get thetaMin() { return this._thetaMin; }
+    get thetaMax() { return this._thetaMax; }
 
     integrate(func) {
         let sum = 0;
         for (let index = 0; index < this.count; index++) {
             const {theta, phi} = this.thetaPhiAt(index);
             if (this.segmentVisibleAt(index))
-                sum += func(theta, phi) * Math.sin(this._thetaPhi[index].theta);
+                sum += func(theta, phi) * Math.sin(theta);
         }
         return sum * R * R * da * da;
     }
-
-    get minF() { return this._minF; }
-    get maxF() { return this._maxF; }
 
     thetaPhiAt(index) { return this._thetaPhi[index]; }
 }
 const sphere = new SegmentedSphere();
 
-const updateIntegral = () =>
-    document.getElementById("integral-title").textContent = "Integral evaluates to: " + sphere.integrate(views).toFixed(2);
-
 const sphereView = new BoxSegmentsView({
-    opacity: 0.35,
+    opacity: 0.75,
     colorMapper: (segment, index, targetColor) => {
         const {theta, phi} = sphere.thetaPhiAt(index);
         const value = functionToIntegrate(theta, phi);
-        const t = (value - sphere.minF) / (sphere.maxF - sphere.minF);
-        targetColor.setRGB(1 - t, t, 0);
+        const t = sphere.normalize(value);
+        targetColor.setRGB(.2 - t, t, t * .25);
     },
     visibilityMapper: (segment, index) => sphere.segmentVisibleAt(index)
 });
@@ -89,9 +108,30 @@ const simulation = Simulation
     .append(new Slider("θ_min")
         .on(sphere)
         .withProperty("thetaMin")
-        .withValue(0)
         .withRange(new Range(0, 180, 1))
+        .withValue(sphere.thetaMin)
+    )
+    .append(new Slider("θ_max")
+        .on(sphere)
+        .withProperty("thetaMax")
+        .withRange(new Range(0, 180, 1))
+        .withValue(sphere.thetaMax)
+    )
+    .append(new Slider("φ_min")
+        .on(sphere)
+        .withProperty("phiMin")
+        .withRange(new Range(0, 360, 1))
+        .withValue(sphere.phiMin)
+    )
+    .append(new Slider("φ_max")
+        .on(sphere)
+        .withProperty("phiMax")
+        .withRange(new Range(0, 360, 1))
+        .withValue(sphere.phiMax)
     );
+
+simulation._viewport.controlsDiv.append(integralValueDiv);
+sphere.updateIntegral(sphere.integrate(functionToIntegrate).toFixed(2));
 
 // TODO
 const boundingBox = new Box3();
