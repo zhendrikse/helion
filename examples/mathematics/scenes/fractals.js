14b1937c383264f0a8d070521dc4808e9b254e68
Zeger Hendrikse <zegerh@yahoo.co.uk>
2026-08-15 17:08:14 +0200

    Refactoring as preparation for new apps ⚒️

---
+++ examples/mathematics/scenes/fractals.js
@@ -0,0 +1,286 @@
+import { Color } from "three";
+import {Button, degToRad, hsvToRgb, LineSegments, LineSegmentsView, Simulation, Vec3} from "../../../src/index.js";
+
+class Turtle extends LineSegments {
+    static PenState = Object.freeze({
+        UP: false,
+        DOWN: true
+    });
+
+    constructor({
+        penState = Turtle.PenState.UP,
+        color = new Color(0xffff00)
+    } = {}) {
+        super();
+
+        this.currentColor = color;
+        this.penState = penState;
+
+        this.angle = 0;
+        this.x = 0;
+        this.y = 0;
+    }
+
+    right(angle) {
+        this.angle += degToRad(angle);
+        return this;
+    }
+
+    left(angle) {
+        this.angle -= degToRad(angle);
+        return this;
+    }
+
+    backward(distance) {
+        return this.forward(-distance);
+    }
+
+    penDown() {
+        this.penState = Turtle.PenState.DOWN;
+        return this;
+    }
+
+    penUp() {
+        this.penState = Turtle.PenState.UP;
+        return this;
+    }
+
+    color(color) {
+        this.currentColor = color;
+        return this;
+    }
+
+    forward(distance) {
+        const newX = this.x + distance * Math.cos(this.angle);
+        const newY = this.y + distance * Math.sin(this.angle);
+
+        this.goto(newX, newY);
+        return this;
+    }
+
+    goto(x, y) {
+        const from = new Vec3(this.x, this.y, 0);
+        const to = new Vec3(x, y, 0);
+
+        if (this.penState === Turtle.PenState.DOWN)
+            this.add(from, to, this.currentColor);
+
+        this.x = x;
+        this.y = y;
+
+        return this;
+    }
+}
+
+function kochSnowflake(turtle, length, depth) {
+    if (depth === 0) {
+        turtle.forward(length);
+    } else {
+        length /= 3.0;
+
+        kochSnowflake(turtle, length, depth - 1);
+
+        turtle.left(60);
+        kochSnowflake(turtle, length, depth - 1);
+
+        turtle.right(120);
+        kochSnowflake(turtle, length, depth - 1);
+
+        turtle.left(60);
+        kochSnowflake(turtle, length, depth - 1);
+    }
+}
+
+function drawSnowflake(turtle) {
+    turtle.clear();
+    turtle.goto(-150, 90);
+    turtle.penDown();
+    turtle.color(new Color(0xffff00));
+
+    for (let i = 0; i < 3; i++)
+        kochSnowflake(turtle, 300, 4);
+
+    turtle.right(120);
+}
+
+function cesaroFractal(turtle, depth, length) {
+    if (depth === 0) {
+        turtle.forward(length);
+    } else {
+        const angles = [85, -170, 85, 0];
+
+        for (const angle of angles) {
+            cesaroFractal(turtle, depth - 1, length / 3);
+            turtle.left(angle);
+        }
+    }
+}
+
+function drawCesaro(turtle) {
+    turtle.goto(10, 300);
+    turtle.penDown();
+    turtle.color(new Color(0x00ff00));
+
+    cesaroFractal(turtle, 6, 4000);
+}
+
+function sierpinskiTriangle(turtle, length, depth) {
+    if (depth === 0) {
+        for (let i = 0; i < 3; i++) {
+            turtle.forward(length);
+            turtle.left(120);
+        }
+    } else {
+        sierpinskiTriangle(turtle, length / 2, depth - 1);
+
+        turtle.forward(length / 2);
+
+        sierpinskiTriangle(turtle, length / 2, depth - 1);
+
+        turtle.backward(length / 2);
+        turtle.left(60);
+        turtle.forward(length / 2);
+        turtle.right(60);
+
+        sierpinskiTriangle(turtle, length / 2, depth - 1);
+
+        turtle.left(60);
+        turtle.backward(length / 2);
+        turtle.right(60);
+    }
+}
+
+function drawSierpinskiTriangle(turtle) {
+    turtle.goto(50, 375);
+    turtle.penDown();
+    turtle.color(new Color(0xffffff));
+
+    sierpinskiTriangle(turtle, 400, 5);
+}
+
+function dragonCurve(n) {
+    if (n === 0)
+        return [[0, 0], [1, 0]];
+
+    const prevPoints = dragonCurve(n - 1);
+    const newPoints = [];
+
+    for (let i = 0; i < prevPoints.length - 1; i++) {
+        const [x1, y1] = prevPoints[i];
+        const [x2, y2] = prevPoints[i + 1];
+
+        const dx = x2 - x1;
+        const dy = y2 - y1;
+
+        let newDx;
+        let newDy;
+
+        if (i % 2 === 0) {
+            newDx = dy;
+            newDy = -dx;
+        } else {
+            newDx = -dy;
+            newDy = dx;
+        }
+
+        const newX = x1 + dx / 2 + newDx / 2;
+        const newY = y1 + dy / 2 + newDy / 2;
+
+        newPoints.push(
+            [x1, y1],
+            [newX, newY]
+        );
+    }
+
+    newPoints.push(prevPoints[prevPoints.length - 1]);
+
+    return newPoints;
+}
+
+function drawDragonCurve(turtle) {
+    turtle.clear();
+    const points = dragonCurve(12);
+
+    const scale = 400;
+    const offsetX = 0;
+    const offsetY = 0;
+
+    const [x0, y0] = points[0];
+
+    turtle.penUp();
+    turtle.goto(x0 * scale + offsetX, y0 * scale + offsetY);
+    turtle.penDown();
+
+    for (let i = 1; i < points.length; i++) {
+        const [x, y] = points[i];
+        const { r, g, b } = hsvToRgb(i / points.length, 1, 1);
+        turtle.color(new Color(r, g, b));
+        turtle.goto(
+            x * scale + offsetX,
+            y * scale + offsetY
+        );
+    }
+}
+
+function tSquare(turtle, n, x, y, w) {
+    if (n === 0)
+        return;
+
+    const { r, g, b } = hsvToRgb(w * 0.5, 1, 1);
+    turtle.color(new Color(r, g, b));
+
+    turtle.goto(x, y);
+    turtle.penDown();
+
+    turtle.goto(x + w, y);
+    turtle.goto(x + w, y + w);
+    turtle.goto(x, y + w);
+    turtle.goto(x, y);
+
+    turtle.penUp();
+
+    const newW = w / 3;
+
+    tSquare(turtle, n - 1, x + newW, y + newW, newW);
+    tSquare(turtle, n - 1, x + newW, y + newW * 5, newW);
+    tSquare(turtle, n - 1, x + newW * 5, y + newW * 5, newW);
+    tSquare(turtle, n - 1, x + newW * 5, y + newW, newW);
+}
+
+function drawTSquare(turtle) {
+    turtle.clear();
+    const n = 7;
+    const w = 2 ** n;
+
+    turtle.color(new Color(0xff0000));
+    turtle.penUp();
+    turtle.goto(0, 0);
+
+    tSquare(turtle, n, 0, 0, w);
+}
+
+const turtle = new Turtle();
+const turtleView = new LineSegmentsView();
+
+drawDragonCurve(turtle);
+Simulation
+    .with({
+        htmlDivId: "fractalsContainer",
+        fieldOfView: 40,
+        cameraPosition: new Vec3(0, 0, 1000),
+        controlsTarget: new Vec3(+150, +0, 0),
+        parameterMenuCollapsed: false
+    })
+    .bind(turtle.alwaysWith(turtleView))
+    .append(new Button()
+        .withText("T-square fractal 🔶")
+        .addEventListener("click", () => drawTSquare(turtle))
+        .togetherWith(new Button()
+            .withText("Dragon curve 🐦‍🔥")
+            .addEventListener("click", () => drawDragonCurve(turtle))
+        )
+    )
+    .append(new Button()
+        .withText("Koch snowflake ❄️")
+        .addEventListener("click", () => drawSnowflake(turtle))
+    )
---
+++ examples/waves/scenes/suspended_spring.js
@@ -0,0 +1,90 @@
+import {
+    Vec3, Simulation, Sphere, Floor, SwitchableBondView, Box, Block,
+    Slider, Range, Vec2, Lattice, LatticeView, ChainTopology, UniformGravitationalForce, DragForce,
+} from "../../../src/index.js";
+import 'uplot/dist/uPlot.min.css';
+
+const count = 50;
+const chainLength = 10;
+const chain = new Lattice({
+    k:  30,
+    damping: 0.2
+})
+    .apply(new ChainTopology({
+        count: count,
+        length: chainLength,
+        bondRestLength: 0.99 * chainLength / (count - 1)
+    }))
+    .fixateBodyAt(0)
+    .fixateBodyAt(count - 1);
+
+const latticeView = LatticeView.from({
+    bodyView: Sphere,
+    bondView: SwitchableBondView,
+    bodyArgs: {
+        castShadow: true
+    },
+    bondArgs: {
+        thickness: 0.075,
+        coils: 6,
+        color: 0xffff99,
+        castShadow: true,
+        tubularSegments: 400,
+        bondType: SwitchableBondView.Type.Spring,
+        radiusFunction: pair => .4 * (pair.body1.radius + pair.body2.radius)
+    }
+});
+
+const pole1 = new Block({
+    size: new Vec3(0.175, 2.85, 0.175),
+    position: new Vec3(-5, -1.25, 0)
+})
+const pole2 = new Block({
+    size: new Vec3(0.175, 2.85, 0.175),
+    position: new Vec3(5, -1.25, 0)
+})
+
+const gravitationalForce = new UniformGravitationalForce();
+Simulation
+    .with({
+        htmlDivId: "suspendedSpringContainer",
+        cameraPosition: new Vec3(-10, .25, 5).multiplyScalar(1.25),
+        controlsTarget: new Vec3(-1.5, -1.5, 0),
+        shadowsEnabled: true,
+        fieldOfView: 20,
+        background: Simulation.Background.FOG,
+        headUpDisplay: true
+    })
+    .withMouseClickEventListener()
+    .runsEvery(1e-3)
+    .substeps(40)
+    .advancesBy(5e-4)
+    .onStep((clock, dt) => {
+        chain
+            .applyToBodies(new DragForce(-1e-4))
+            .applyToBodies(gravitationalForce)
+            .integrate(dt);
+    })
+    .bind(chain.alwaysWith(latticeView))
+    .bind(pole1.onceWith(new Box({ color: 0x855E42 })))
+    .bind(pole2.onceWith(new Box({ color: 0x855E42 })))
+    .addObject3D(new Floor({
+        type: Floor.Type.WOOD_WICKER,
+        position: new Vec3(0, -2.75, 0),
+        planeSizeXy: new Vec2(200, 200),
+        granularity: 20
+    }))
+    .append(new Slider("Bond force ")
+        .on(chain)
+        .withProperty("bondForce")
+        .withRange(new Range(10, 1000, 1))
+        .withValue(30)
+    )
+    .append(new Slider("Damping ")
+        .on(chain)
+        .withProperty("damping")
+        .withRange(new Range(0, .5, .01))
+        .withValue(0.2)
+    );
+
+latticeView.nodesVisible = false;
