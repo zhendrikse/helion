78ad8d4bc7ac3368f8003b48fc1af0871d135160
Zeger Hendrikse <zegerh@yahoo.co.uk>
2026-08-15 13:27:50 +0200

    Bug fix 🐞

--- src/model/math/numerics/integrators/integrators.js
+++ src/model/math/numerics/integrators/integrators.js
@@ -24,26 +24,28 @@ export class Integrators {
         physicsState.velocity.addScaledVector(k1.dv.clone().add(k2.dv), dt / 2);
     }
 
-    static rk4Step(physicsState, dt, derivativeFn = state => ({
-        dx: state.velocity.clone(),
-        dv: state.acceleration
-    })) {
-        const k1 = derivativeFn(physicsState);
+    static rk4Step(physicsState, dt) {
+        const derivative = body => ({
+            dx: body.velocity.clone(),
+            dv: body.acceleration.clone()
+        });
+
+        const k1 = derivative(physicsState);
         const s2 = physicsState.clone();
         s2.position.addScaledVector(k1.dx, dt / 2);
         s2.velocity.addScaledVector(k1.dv, dt / 2);
 
-        const k2 = derivativeFn(s2);
+        const k2 = derivative(s2);
         const s3 = physicsState.clone();
         s3.position.addScaledVector(k2.dx, dt / 2);
         s3.velocity.addScaledVector(k2.dv, dt / 2);
 
-        const k3 = derivativeFn(s3);
+        const k3 = derivative(s3);
         const s4 = physicsState.clone();
         s4.position.addScaledVector(k3.dx, dt);
         s4.velocity.addScaledVector(k3.dv, dt);
 
-        const k4 = derivativeFn(s4);
+        const k4 = derivative(s4);
         physicsState.position
             .addScaledVector(k1.dx, dt / 6)
             .addScaledVector(k2.dx, dt / 3)
