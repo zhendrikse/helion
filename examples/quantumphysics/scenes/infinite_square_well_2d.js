f8fdd37f5da2e5f76642d81ccd770ddad1ba3e89
Zeger Hendrikse <zegerh@yahoo.co.uk>
2026-08-23 22:38:46 +0200

    Code clean-up ️️🧹

--- examples/quantumphysics/scenes/infinite_square_well_2d.js
+++ examples/quantumphysics/scenes/infinite_square_well_2d.js
@@ -1,32 +1,16 @@
-import { Mesh, MeshPhongMaterial, CylinderGeometry} from "three";
 import {
-    Complex, DiscreteComplexField, Renderable3D, Simulation, Vec3, WaveFunctionEigenStateSolver
+    Complex, Cylinder, DiscreteComplexField, Renderable3D, Simulation, Vec3, WaveFunctionEigenStateSolver
 } from "../../../src/index.js";
 
 class DiscreteComplexFieldCylinderView extends Renderable3D {
     constructor({
-        size = 20,
         spacing = 10,
         cylinderScale = 20
     } = {}) {
         super();
-        this._x = [];
-        this._y = [];
         this._spacing = spacing;
-        this._size = size;
         this._cylinderScale = cylinderScale;
-
-        for (let i = 0; i < size; i++) {
-            this._x[i] = [];
-            this._y[i] = [];
-
-            for (let j = 0; j < size; j++) {
-                this._x[i][j] = spacing * i / (size - 1);
-                this._y[i][j] = spacing * j / (size - 1);
-            }
-        }
-
-        this._cylinders = this._createCylinders(size, spacing);
+        this._cylinders = [];
         this._complexNumber = new Complex();
     }
 
@@ -37,50 +21,37 @@ class DiscreteComplexFieldCylinderView extends Renderable3D {
     }
 
     synchronizeWith(psi) {
-        for (let i = 0; i < this._size; i++)
-            for (let j = 0; j < this._size; j++)
+        for (let i = 0; i < psi.nx; i++)
+            for (let j = 0; j < psi.ny; j++)
                 this._updateCylinder(psi, i, j);
     }
 
     _updateCylinder(psi, i, j) {
         psi.valueAt(i, j, this._complexNumber);
         this._complexNumber.multiplyScalar(this._cylinderScale);
-        const mesh = this._cylinders[i][j];
         const height = this._complexNumber.re;
         const mag = this._complexNumber.abs;
         const radius = Math.max(0.05 * mag, Math.abs(this._complexNumber.im) / 6);
-        const phase = this._complexNumber.phase;
+        const hue = this._complexNumber.phase;
 
-        mesh.scale.set(radius, Math.abs(height), radius);
-        mesh.position.y = height / 2;
-        mesh.material.color.setHSL(phase, 1, 0.5);
+        const cylinder = this._cylinders[psi.index(i, j)];
+        const x = this._spacing * i / (psi.nx - 1);
+        const y = this._spacing * j / (psi.ny - 1)
+        cylinder._mesh.position.set(x - this._spacing / 2, height/2, y - this._spacing / 2);
+        cylinder._mesh.scale.set(radius, Math.abs(height), radius);
+        cylinder._mesh.material.color.setHSL(hue, 1, 0.5);
     }
 
-    _createCylinders(size, spacing) {
-        const cylinders = [];
-        const geometry = new CylinderGeometry(1, 1, 1, 12);
-
-        for (let i = 0; i < size; i++) {
-            cylinders[i] = [];
-            for (let j = 0; j < size; j++) {
-                const mesh = this._createCylinder(i, j, spacing, geometry);
-                this.add(mesh);
-                cylinders[i][j] = mesh;
+    initialize(psi) {
+        for (let i = 0; i < psi.nx; i++)
+            for (let j = 0; j < psi.ny; j++) {
+                this._cylinders[psi.index(i, j)] = new Cylinder();
+                this.add(this._cylinders[psi.index(i, j)]);
             }
-        }
-
-        return cylinders;
-    }
-
-    _createCylinder(i, j, spacing, geometry) {
-        const material = new MeshPhongMaterial({ color: 0xff0000 });
-        const mesh = new Mesh(geometry, material);
-        mesh.position.set(this._x[i][j] - spacing / 2, 0, this._y[i][j] - spacing / 2,);
-        return mesh;
     }
 }
 
-const waveFunctionView = new DiscreteComplexFieldCylinderView({ size: 20, spacing: 10 });
+const waveFunctionView = new DiscreteComplexFieldCylinderView({ spacing: 10 });
 const waveFunctionPsi = new DiscreteComplexField({ nx: 20, ny: 20 });
 const solver = new WaveFunctionEigenStateSolver();
 waveFunctionPsi.evolve(solver, 0.01);
@@ -106,4 +77,3 @@ Simulation.with({
     .withMouseClickEventListener()
     .runsEvery(0.01)
     .onStep((_, dt) => waveFunctionPsi.evolve(solver, dt));
-
