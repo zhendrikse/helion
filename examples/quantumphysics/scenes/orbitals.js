e3c53e18d0830e57be57cfcc50480d2c4e98bc45
Zeger Hendrikse <zegerh@yahoo.co.uk>
2026-09-08 22:09:40 +0200

    Code clean-up 🗑️

--- examples/quantumphysics/scenes/orbitals.js
+++ examples/quantumphysics/scenes/orbitals.js
@@ -16,51 +16,63 @@ const orbitals = {
         latex: "1s"
     },
     "2pₓ": {
-        definition: orbitalSurface((theta, phi) => Math.abs(Math.sin(theta) * Math.cos(phi))),
+        definition: orbitalSurface((/** @type {number} */ theta, /** @type {number} */ phi) => 
+            Math.abs(Math.sin(theta) * Math.cos(phi))),
         latex: "2p_x"
     },
     "2pᵧ": {
-        definition: orbitalSurface((theta, phi) => Math.abs(Math.sin(theta) * Math.sin(phi))),
+        definition: orbitalSurface((/** @type {number} */ theta, /** @type {number} */ phi) => 
+            Math.abs(Math.sin(theta) * Math.sin(phi))),
         latex: "2p_y"
     },
     "2p_z": {
-        definition: orbitalSurface(theta => Math.abs(Math.cos(theta))),
+        definition: orbitalSurface((/** @type {number} */ theta) => 
+            Math.abs(Math.cos(theta))),
         latex: "2p_z"
     },
     "3d_z²": {
-        definition: orbitalSurface(theta => Math.abs(3 * Math.cos(theta) ** 2 - 1)),
+        definition: orbitalSurface((/** @type {number} */ theta) => 
+            Math.abs(3 * Math.cos(theta) ** 2 - 1)),
         latex: "3d_{z^2}"
     },
     "3d_xz": {
-        definition: orbitalSurface((theta, phi) => Math.abs(Math.sin(theta) * Math.cos(theta) * Math.cos(phi))),
+        definition: orbitalSurface((/** @type {number} */ theta, /** @type {number} */ phi) => 
+            Math.abs(Math.sin(theta) * Math.cos(theta) * Math.cos(phi))),
         latex: "3d_{xz}"
     },
     "3d_yz": {
-        definition: orbitalSurface((theta, phi) => Math.abs(Math.sin(theta) * Math.cos(theta) * Math.sin(phi))),
+        definition: orbitalSurface((/** @type {number} */ theta, /** @type {number} */ phi) => 
+            Math.abs(Math.sin(theta) * Math.cos(theta) * Math.sin(phi))),
         latex: "3d_{yz}"
     },
     "3d_x²₋z²": {
-        definition: orbitalSurface((theta, phi) => Math.abs(Math.sin(theta) ** 2 * Math.cos(2 * phi))),
+        definition: orbitalSurface((/** @type {number} */ theta, /** @type {number} */ phi) => 
+            Math.abs(Math.sin(theta) ** 2 * Math.cos(2 * phi))),
         latex: "3d_{x^2-z^2}"
     },
     "3d_xy": {
-        definition: orbitalSurface((theta, phi) => Math.abs(Math.sin(theta) ** 2 * Math.sin(2 * phi))),
+        definition: orbitalSurface((/** @type {number} */ theta, /** @type {number} */ phi) => 
+            Math.abs(Math.sin(theta) ** 2 * Math.sin(2 * phi))),
         latex: "3d_{xy}"
     },
     "4f_z³": {
-        definition: orbitalSurface(theta => Math.abs(5 * Math.cos(theta) ** 3 - 3 * Math.cos(theta))),
+        definition: orbitalSurface((/** @type {number} */ theta) => 
+            Math.abs(5 * Math.cos(theta) ** 3 - 3 * Math.cos(theta))),
         latex: "4f_{z^3}"
     },
     "4f_xyz": {
-        definition: orbitalSurface((theta, phi) => 2 * Math.abs(Math.sin(theta) ** 2 * Math.cos(theta) * Math.sin(2 * phi))),
+        definition: orbitalSurface((/** @type {number} */ theta, /** @type {number} */ phi) => 
+            2 * Math.abs(Math.sin(theta) ** 2 * Math.cos(theta) * Math.sin(2 * phi))),
         latex: "4f_{xyz}"
     },
     "4f_x(x²−3z²)": {
-        definition: orbitalSurface((theta, phi) => Math.abs(Math.sin(theta) ** 3 * Math.cos(3 * phi))),
+        definition: orbitalSurface((/** @type {number} */ theta, /** @type {number} */ phi) => 
+            Math.abs(Math.sin(theta) ** 3 * Math.cos(3 * phi))),
         latex: "4f_{x(x^2-3z^2)}"
     },
     "4f_z(x²−z²)": {
-        definition: orbitalSurface((theta, phi) => Math.abs(Math.sin(theta) ** 3 * Math.sin(3 * phi))),
+        definition: orbitalSurface((/** @type {number} */ theta, /** @type {number} */ phi) => 
+            Math.abs(Math.sin(theta) ** 3 * Math.sin(3 * phi))),
         latex: "4f_{z(x^2-z^2)}"
     }
 };
@@ -81,18 +93,20 @@ const simulation = Simulation.with({
     parameterMenuCollapsed: false
 });
 
-function showOrbital(name) {
-    const surface = orbitalRegistry.get(name).definition;
+/** @param {string} orbitalName */
+function showOrbital(orbitalName) {
+    const surface = orbitalRegistry.get(orbitalName).definition;
     simulation.bind(surface.onceWith(surfaceView));
     simulation.provideAxesAround(surfaceView, {tickLabels: false, annotations: false});
     simulation.frameSceneOn(surfaceView, {padding: 0.8});
-    simulation.setLatexTitle("\\Large{" + orbitalRegistry.get(name).latex + " }\\ \\text{orbital}");
+    simulation.setLatexTitle("\\Large{" + orbitalRegistry.get(orbitalName).latex + " }\\ \\text{orbital}");
 }
 
 simulation
     .append(new DropdownMenu()
         .for(orbitalRegistry)
         .withValue("2p_z")
+        // @ts-ignore
         .onChange(event => showOrbital(event.target.value)));
 
 showOrbital("2p_z");
