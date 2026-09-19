af48ff30f442b8f5e832687f80e1ace54df112d6
Zeger Hendrikse <zegerh@yahoo.co.uk>
2026-09-19 12:17:12 +0200

    Add first simple lattice Boltzmann fluid demo

---
+++ examples/fluiddynamics/scenes/lattice_boltzmann_2d.js
@@ -0,0 +1,74 @@
+import {
+    AdaptiveSymmetricNormalizer, Button, Checkbox, ColorMappers, Colour, FixedIntervalNormalizer,
+    Interval, LatticeBoltzmannFluid2D, Simulation, Slider, TiledPlane, Vec3
+} from '../../../src/index.js';
+
+const NX = 120;
+const NY = 60;
+const CELL_SIZE = 0.08;
+
+const fluid = new LatticeBoltzmannFluid2D({
+    nx: NX,
+    ny: NY,
+    viscosity: 0.02,
+    flowSpeed: 0.10
+});
+
+const curlView = new TiledPlane({
+    cellSize: CELL_SIZE,
+    colorMapper: ColorMappers.get(ColorMappers.Seismic),
+    normalizer: new AdaptiveSymmetricNormalizer(0.08)
+});
+
+const barrierView = new TiledPlane({
+    cellSize: CELL_SIZE,
+    colorMapper: ColorMappers.get(ColorMappers.Uniform, { color: Colour.Black.asHexValue() }),
+    normalizer: new FixedIntervalNormalizer(new Interval(0, 1)),
+    opacityFunction: value => value
+});
+
+barrierView.position.z = 0.01;
+
+const simulation = Simulation
+    .with({
+        htmlDivId: 'latticeBoltzmann2dContainer',
+        camera: {
+            position: new Vec3(0, 0, 10),
+            orthographic: true,
+            controls: false
+        },
+        lighting: { enabled: false },
+        infoPanel: {
+            text:
+                '<strong>🌊 2D lattice-Boltzmann fluid</strong><br/>' +
+                'A simple D2Q9 fluid flowing from left to right around a barrier. ' +
+                'The background shows vorticity (curl).'
+        }
+    })
+    .runsEvery(0.01)
+    .onStep(() => {
+        // A few lattice steps per simulation tick keeps the flow moving smoothly.
+        fluid.evolve();
+        fluid.evolve();
+    })
+    .appendStartStopResetUI()
+    .append(new Slider('Viscosity')
+        .withRange(new Interval(0.005, 0.08))
+        .withValue(0.02)
+        .onInput(event => {
+            // @ts-ignore
+            fluid.viscosity = Number(event.target.value);
+        }))
+    .append(new Checkbox('Show barrier')
+        .checked(true)
+        .addEventListener('change', event => {
+            // @ts-ignore
+            barrierView.visible = event.target.checked;
+        }))
+    .append(new Button()
+        .withText('Reset')
+        .onClick(() => fluid.reset()))
+    .start();
+
+simulation.bind(fluid.curlField.alwaysWith(curlView));
+simulation.bind(fluid.barrierField.alwaysWith(barrierView));
