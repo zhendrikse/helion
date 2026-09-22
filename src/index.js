e1cc14f7c2e6bfa56ddf82bc8826783d8c9c34f4
Zeger Hendrikse <zegerh@yahoo.co.uk>
2026-09-22 12:39:16 +0200

    Code clean-up ️🧹

--- src/index.js
+++ src/index.js
@@ -68,11 +68,10 @@ export { ArrowField2D } from './view/2d/composite/composites.js';
 export { WaveEquationSolver, JacobiSolver } from './model/math/numerics/solvers/solvers.js';
 
 export { SchrodingerEigenstateSolver3D } from './model/math/numerics/solvers/qmsolvers.js';
-export { SchrodingerSolver, SchrodingerEigenstateSolver } from './model/math/quantum/schrodinger.js';
-
-export { Hamiltonian } from './model/math/quantum/hamiltonian.js';
-export { SingleParticle } from './model/math/quantum/particles.js';
-export { InfiniteSquareWell2D } from './model/math/quantum/infinite_square_well.js';
+export { SchrodingerSolver, SchrodingerEigenstateSolver } from './model/phys/quantum/schrodinger.js';
+export { Hamiltonian } from './model/phys/quantum/hamiltonian.js';
+export { SingleParticle } from './model/phys/quantum/particles.js';
+export { InfiniteSquareWell2D } from './model/phys/quantum/infinite_square_well.js';
 export { DirichletBoundaryCondition } from './model/math/numerics/boundaryconditions/dirichlet.js';
 
 export {
