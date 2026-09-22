6e00eca46d74a3b167f5301c77cc7e1110fb7bed
Zeger Hendrikse <zegerh@yahoo.co.uk>
2026-09-22 21:22:24 +0200

    Export standard quantum potentials

--- src/index.js
+++ src/index.js
@@ -70,6 +70,7 @@ export { WaveEquationSolver, JacobiSolver } from './model/math/numerics/solvers/
 export { SchrodingerEigenstateSolver3D } from './model/math/numerics/solvers/qmsolvers.js';
 export { SchrodingerSolver, SchrodingerEigenstateSolver } from './model/phys/quantum/schrodinger.js';
 export { Hamiltonian } from './model/phys/quantum/hamiltonian.js';
+export { Potentials, infiniteSquareWell, harmonicOscillator, anisotropicHarmonicOscillator, doubleWell, circularWell, quartic } from './model/phys/quantum/potentials.js';
 export { SingleParticle } from './model/phys/quantum/particles.js';
 export { InfiniteSquareWell2D } from './model/phys/quantum/infinite_square_well.js';
 export { DirichletBoundaryCondition } from './model/math/numerics/boundaryconditions/dirichlet.js';
